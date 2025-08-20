d77883660371d19350577ea73d75ac074ab46baa
marcelomarmelo <marcelomatheospelizario@gmail.com>
2025-08-20 17:29:13 -0300

    programando o arquivo materias.php

---
+++ rotas/materias.php
@@ -0,0 +1,19 @@
+<?php
+/*cabeçalhos do php */
+header("Content-Type: application/json; charset=UTF-8");
+header("Acess-Control-Allow-Origin: *");
+header("Acess-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+/*essa variavel recebe o metodo utilizado pode ser post,get,put ou delete
+*/
+$metodoSolicitado=$_SERVER['REQUEST_METHOD'];
+/* esse id é quando colocamos informações na url*/
+$id = $_GET['id'] ?? null
+/* ?? significa que se $_GET['id'] existir e nao for nula o conteudo entra na variável id */
+switch($metodoSolicitado){
+    case "POST";
+    $dados_recebidos = json_decode(file_get_contents("php://input"),true);
+    break;
+    case "GET";
+    echo "Veio do navegador";
+}
+?>
