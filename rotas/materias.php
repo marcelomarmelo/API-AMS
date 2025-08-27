4acfc09df7ffa01590381c3abec274848d75ebb6
marcelomarmelo <marcelomatheospelizario@gmail.com>
2025-08-27 17:27:39 -0300

    finalização da api, e ligação com o banco de dados

--- rotas/materias.php
+++ rotas/materias.php
@@ -5,15 +5,34 @@ header("Acess-Control-Allow-Origin: *");
 header("Acess-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
 /*essa variavel recebe o metodo utilizado pode ser post,get,put ou delete
 */
-$metodoSolicitado=$_SERVER['REQUEST_METHOD'];
+$metodoSolicitado = $_SERVER['REQUEST_METHOD'];
 /* esse id é quando colocamos informações na url*/
-$id = $_GET['id'] ?? null
+$id = $_GET['id'] ?? null;
 /* ?? significa que se $_GET['id'] existir e nao for nula o conteudo entra na variável id */
 switch($metodoSolicitado){
-    case "POST";
+    case "POST":
     $dados_recebidos = json_decode(file_get_contents("php://input"),true);
     break;
-    case "GET";
-    echo "Veio do navegador";
+    case "GET":
+
+        $servidor = "localhost"; 
+        $usuario = "root"; 
+        $senha = ""; 
+        $banco = "escola"; 
+
+        $conexao = new mysqli($servidor,$usuario,$senha,$banco);
+
+        $sql = "Select * from Materias";
+
+        $resultado = $conexao->query($sql);
+
+        $usuarios = [];
+        while($linha = $resultado->fetch_assoc())
+        {
+            $materias[] = $linha;
+        }
+
+        echo json_encode($materias);
+    break;
 }
 ?>
